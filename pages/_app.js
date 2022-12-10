b8d4c9e2ae13194ae2b61534bce549884947444a
hasindu sithmin <hasindusithmin64@gmail.com>
2022-12-10 07:59:12 +0000

    Update pages/index.js & pages/_app.js

--- pages/_app.js
+++ pages/_app.js
@@ -8,7 +8,10 @@ function MyApp({ Component, pageProps }) {
   return (
     <>
       <Navbar/>
-      <Component {...pageProps} />
+      <div className="w3-main w3-content w3-padding" style={{maxWidth:'1200px',marginTop:'100px'}}>
+        <Component {...pageProps} />
+      </div>
+
     </>
   )
 }
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,21 @@
+import { useEffect, useState } from "react"
+
+
 
 export default function Home() {
+    const [page,setPage] = useState(1)
+    const [movies,setMovies] = useState(null)
+    useEffect(()=>{
+      fetch(`https://yts.torrentbay.to/api/v2/list_movies.json`)
+        .then(res=>res.json())
+        .then(data=>{
+          setMovies(data)
+        })
+    },[])
 
     return (
-      <div></div>
+      <div className="w3-row-padding w3-padding-16 w3-center">
+
+      </div>
     )
 }
